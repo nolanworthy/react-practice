4ca479930e779d10dc1f066e4a99297433c775f9
nolanworthy <nolan@worthyplace.com>
2021-08-15 23:39:22 -0400

    added components subdirectory in src and created Button, Header, Task, and Tasks components

---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import Task from './Task'
+
+const Tasks = ({ tasks }) => {
+    return (
+        <>
+            {tasks.map((task) => {
+                return (<Task key={task.id} task={task}/>);
+            })}
+        </>
+    );
+}
+
+export default Tasks;
