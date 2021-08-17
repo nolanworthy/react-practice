205ad9d3906c14e76a6379f9c134f3155fece74f
nolanworthy <nolan@worthyplace.com>
2021-08-16 22:22:42 -0400

    added functionality to add a new task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,6 +25,13 @@ function App() {
     }
   ]);
 
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
   //delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -38,6 +46,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks
           tasks={tasks}
