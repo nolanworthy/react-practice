4238f2324d61826918fcc910b49bfc815cc3924d
nolanworthy <nolan@worthyplace.com>
2021-08-16 21:35:16 -0400

    passing onDelete as prop from App.js down to Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,10 @@
 import Task from './Task'
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
     return (
         <>
             {tasks.map((task) => {
-                return (<Task key={task.id} task={task}/>);
+                return (<Task key={task.id} task={task} onDelete={onDelete}/>);
             })}
         </>
     );
